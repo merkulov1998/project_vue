e7dee76a4ac0df9005f67a2600bff7b7ab69fea5
Andrey <andrew.merkulov2011@yandex.ru>
2023-08-29 23:30:35 +0300

    creating posts

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div class="post" v-for="post in posts">
+    <div><strong>Название:</strong>{{ post.title }}</div>
+    <div><strong>Описание:</strong>{{ post.body }}</div>
+    </div>
 </template>
 
+<script>
+export default {
+    data(){
+        return {
+          posts: [
+            {id: 1, title: 'Javascript', body: 'Описание поста'},
+            {id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
+            {id: 3, title: 'Javascript 3', body: 'Описание поста 3'},
+          ]
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.post{
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
 }
 </style>
