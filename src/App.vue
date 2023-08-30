c6e76d287a5be67f500a4f6f2ede146f16a7ab0c
Andrey <andrew.merkulov2011@yandex.ru>
2023-08-30 09:59:48 +0300

    creating a new post

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
+    <div class="app">
+        <form @submit.prevent>
+            <h4>Создание поста</h4>
+            <input v-bind:value="title" @input= "title = $event.target.value" class="input" type="text" placeholder="Название">
+            <input v-bind:value="body" @input= "body = $event.target.value" class="input" type="text" placeholder="Описание">
+            <button class="btn" @click="createPost">Создать</button>
+        </form>
     <div class="post" v-for="post in posts">
     <div><strong>Название:</strong>{{ post.title }}</div>
     <div><strong>Описание:</strong>{{ post.body }}</div>
+    </div>
     </div>
 </template>
 
@@ -13,11 +21,20 @@ export default {
             {id: 1, title: 'Javascript', body: 'Описание поста'},
             {id: 2, title: 'Javascript 2', body: 'Описание поста 2'},
             {id: 3, title: 'Javascript 3', body: 'Описание поста 3'},
-          ]
+          ],
+          title: '',
+          body: ''
         }
     },
     methods: {
-        
+        createPost(){
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                body: this.body,
+            }
+            this.posts.push(newPost);
+        },
     }
 }
 </script>
@@ -28,10 +45,30 @@ export default {
     padding: 0;
     box-sizing: border-box;
 }
-
+.app{
+    padding: 20px;
+}
 .post{
     padding: 15px;
     border: 2px solid teal;
     margin-top: 15px;
+}
+form{
+    display: flex;
+    flex-direction: column;
+}
+.input{
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+}
+.btn{
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
 }
 </style>
